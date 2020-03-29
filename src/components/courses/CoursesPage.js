9093afb1457f78fd4dce16068eddffb0c2d8e93c
Saurabh <saurabhbharti9@gmail.com>
2020-03-30 02:02:58 +0530

    implementeddispatch actions using bindActionsCreators

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../components/redux/actions/courseActions";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends Component {
   state = {
@@ -17,7 +18,9 @@ class CoursesPage extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    this.props.createCourse(this.state.course);
+    //this.props.dispatch(courseActions.createCourse(this.state.course));
+    //this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
   };
   render() {
     return (
@@ -47,14 +50,21 @@ function mapStateToProps(state) {
   };
 }
 //(Way -1) manually dispatching actions;
-function mapDispatchToProps(dispatch) {
+/**function mapDispatchToProps(dispatch) {
   return {
     createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}*/
+//(Way -2) using bindActionCreators
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   //dispatch: PropTypes.func.isRequired
-  createCourse: PropTypes.func.isRequired
+  //createCourse: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
 };
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
