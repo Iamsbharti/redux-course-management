3dd6322161959eb1907277186f9765b161f783f9
Saurabh <saurabhbharti9@gmail.com>
2020-04-01 06:51:16 +0530

    updated course reducer

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -2,8 +2,12 @@ import * as types from "../actions/actionTypes";
 import initialState from "./initialState";
 export default function courseRducer(state = initialState.courses, action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCEES:
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCEES:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
