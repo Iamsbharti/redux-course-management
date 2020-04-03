26dc737a209a93196a5c87c897084e570788e0cb
Saurabh <saurabhbharti9@gmail.com>
2020-04-04 00:41:46 +0530

    implemented redux-test with Jest/Enzyme on ManageCoursePage

---
+++ src/components/courses/ManageCoursePage.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { mount } from "enzyme";
+import { courses, authors, newCourse } from "../../../tools/mockData";
+import { ManageCoursePage } from "./ManageCoursePage";
+
+function renderManageCoursePage(args) {
+  const defaultProps = {
+    authors,
+    courses,
+    course: newCourse,
+    loadCourses: jest.fn(),
+    saveCourse: jest.fn(),
+    loadAuthors: jest.fn(),
+    history: {},
+    match: {}
+  };
+
+  const props = { ...defaultProps, ...args };
+  return mount(<ManageCoursePage {...props} />);
+}
+it("sets error when attenpting to sumbmit without title", () => {
+  const wrapper = renderManageCoursePage();
+  wrapper.find("form").simulate("submit");
+  const error = wrapper.find(".alert").first();
+  console.log(error.text());
+  expect(error.text()).toBe("Title is required");
+});
