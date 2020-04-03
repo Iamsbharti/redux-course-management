6a029b5cc8ef8de1473c2299d5ab04119336ad16
Saurabh <saurabhbharti9@gmail.com>
2020-04-03 13:21:36 +0530

    implemented Enzyme test suite

---
+++ src/components/courses/CourseForm.Enzyme.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CourseForm from "./CourseForm";
+import { shallow } from "enzyme";
+
+function renderCourseForm(args) {
+  const defaultProps = {
+    authors: [],
+    course: {},
+    saving: false,
+    errors: {},
+    onSave: () => {},
+    onChange: () => {}
+  };
+  const props = { ...defaultProps, ...args };
+  return shallow(<CourseForm {...props} />);
+}
+
+it("should render form and header", () => {
+  const wrapper = renderCourseForm();
+  //console.log(wrapper.debug());
+  expect(wrapper.find("form").length).toBe(1);
+  expect(wrapper.find("h2").text()).toEqual("SaveCourse");
+});
+it("labels save buttons as 'save' when not saving", () => {
+  const wrapper = renderCourseForm();
+  expect(wrapper.find("button").text()).toEqual("Save");
+});
+it("labels save button as 'saving...' while saving", () => {
+  const wrapper = renderCourseForm({ saving: true });
+  expect(wrapper.find("button").text()).toEqual("Saving...");
+});
