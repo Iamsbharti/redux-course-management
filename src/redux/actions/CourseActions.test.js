0e68ea2b29c22265c2778eddd37b36f6fe621565
Saurabh <saurabhbharti9@gmail.com>
2020-04-04 00:59:23 +0530

    added additional test-suites for actions

--- src/redux/actions/CourseActions.test.js
+++ src/redux/actions/CourseActions.test.js
@@ -17,3 +17,49 @@ describe("CreateCourseSuccess", () => {
     expect(action).toEqual(expextedAction);
   });
 });
+
+describe("SaveCourseActions", () => {
+  it("should create action with UPDATE_COURSE_SUCCESS", () => {
+    //arrange
+    const course = courses[0];
+    const expectedAction = {
+      type: types.UPDATE_COURSE_SUCCESS,
+      course
+    };
+
+    //act
+    const action = courseActions.updateCourseSuccess(course);
+
+    //assert
+    expect(action).toEqual(expectedAction);
+  });
+});
+describe("CreateCourseSucces", () => {
+  it("should create an action with CREATE_COURSE_SUCCESS", () => {
+    //act
+    const course = courses[0];
+    const expectedAction = {
+      type: types.CREATE_COURSE_SUCCESS,
+      course
+    };
+    //arrange
+    const action = courseActions.createCourseSuccess(course);
+
+    //assert
+    expect(action).toEqual(expectedAction);
+  });
+});
+describe("LoadCourseSuccess", () => {
+  it("Should create action of type LOAD_COURSE_SUCCESS", () => {
+    //arrange
+    const expectedAction = {
+      type: types.LOAD_COURSES_SUCCESS
+    };
+
+    //act
+    const action = courseActions.loadCourseSuccess();
+
+    //assert
+    expect(action).toEqual(expectedAction);
+  });
+});
